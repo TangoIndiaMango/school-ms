daa43e868bd17b1ab4107dfb2f5d0cd2b1f395d4
TangoIndiaMango <aliyutimileyin2340@gmail.com>
2024-02-13 09:02:11 +0100

    fix: added models to admin dashboard

--- users/admin.py
+++ users/admin.py
@@ -3,4 +3,4 @@ from django.contrib import admin
 from .models import CustomUser, Lecturer, Student
 
 # Register your models here.
-admin.site.register((CustomUser, Student,Lecturer, ))
+admin.site.register((CustomUser, Student, Lecturer, ))
